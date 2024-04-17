7f6d3076fe026e9d7c31813ef20b32acab6805af
Sosnovskikh Kirill <sosnovskix.kir2001@gmail.com>
2024-04-17 22:33:31 +0300

    Updating custom parameters and config

--- pyfactoring/config.py
+++ pyfactoring/config.py
@@ -6,11 +6,19 @@ def load_config():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     cfg_file = os.path.join(parent_dir, "pyproject.toml")
+
     if not os.path.exists(cfg_file):
         return {}
+
     with open(cfg_file, "rb") as f:
         cfg = tomllib.load(f)
-    return cfg.get("tool", {}).get("pyfactoring", {})
+
+    common = cfg.get("tool", {}).get("pyfactoring", {})
+    return {
+        "common": common,
+        "pydioms": common.get("pydioms", {}),
+        "pyclones": common.get("pyclones", {})
+    }
 
 
 config = load_config()
